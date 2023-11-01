92f54335dbac3a358ce1032b0b68f78f99a3a61b
Jens Elstner <juelstner@gmx.de>
2023-11-01 10:45:07 +0100

    Refactored main.c and added instructions to README.md

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # cando
 A library to aid programming with can sockets.
+
+Create a virtual can socket for testing with the script `createVcan.sh`
+
+`  >$ createVcan.sh`
+
+Compile `cando` with `make` and test writing with
+
+`  >$ candump -tz vcan0`
+
+and reading (don't forget to change used function in `maine.c`) with
+
+`  >$ cansend vcan0 123#12345678`
--- main.c
+++ main.c
@@ -7,15 +7,17 @@
 #include "c_string.h"
 
 
+void exitIfError(int iErr, t_can* pcanMan) {
+  if (iErr == C_CANNED_ERR) {
+    perror(pcanMan->csErr.cStr);
+    exit(pcanMan->iErr);
+  }
+}
+
 void writeTest(void) {
   t_can canMan = {0};
-  cstr  csErr  = csNew("");
 
-  if (canNew(&canMan, "vcan0") == C_CANNED_ERR) {
-    csSetf(&csErr, "Error while opening socket: %s", canMan.csErr.cStr);
-    perror(csErr.cStr);
-    exit(canMan.iErr);
-  }
+  exitIfError(canNew(&canMan, "vcan0"), &canMan);
 
   printf("%s at index %d\n", canMan.csIfName.cStr, canMan.ifRequest.ifr_ifindex);
 
@@ -28,27 +30,18 @@ void writeTest(void) {
   canMan.canFrame.data[3] = 0x67;
   canMan.canFrame.data[4] = 0x89;
 
-  canWrite(&canMan);
+  exitIfError(canWrite(&canMan), &canMan);
 
   canFree(&canMan);
 }
 
 void readTest(void) {
   t_can canMan = {0};
-  cstr  csErr  = csNew("");
 
-  if (canNew(&canMan, "vcan0") == C_CANNED_ERR) {
-    csSetf(&csErr, "Error while opening socket: %s", canMan.csErr.cStr);
-    perror(csErr.cStr);
-    exit(canMan.iErr);
-  }
+  exitIfError(canNew(&canMan, "vcan0"), &canMan);
 
   while(1) {
-  if (canRead(&canMan) == C_CANNED_ERR) {
-    csSetf(&csErr, "Error while reading from socket: %s", canMan.csErr.cStr);
-    perror(csErr.cStr);
-    exit(canMan.iErr);
-  }
+    exitIfError(canRead(&canMan), &canMan);
 
     printf("0x%03X [%d] ",canMan.canFrame.can_id, canMan.canFrame.can_dlc);
 
